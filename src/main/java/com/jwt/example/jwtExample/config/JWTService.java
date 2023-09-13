37597ca395f521c708d8c0254d0fbbc7a79bfb98
Rajdeep Ghosh <grajdeep2000@gmail.com>
2023-09-13 15:14:00 +0530

    Created JwtAuthenticationFilter and JwtService

---
+++ src/main/java/com/jwt/example/jwtExample/config/JWTService.java
@@ -0,0 +1,13 @@
+package com.jwt.example.jwtExample.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+
+}
